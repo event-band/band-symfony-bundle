609293593386d932a4636a94ae8b0502a85c6506
Vasil coylOne Kulakov <iam@vasiliy.pro>
2015-10-16 16:04:43 +0300

    Added use_tracer_interceptor config parameter

--- DependencyInjection/EventBandExtension.php
+++ DependencyInjection/EventBandExtension.php
@@ -54,7 +54,7 @@ class EventBandExtension extends ConfigurableExtension
         $this->loader->load('transport/amqp/amqp.xml');
         $this->loader->load(sprintf('transport/amqp/%s.xml', $config['driver']));
 
-        if (class_exists('JMS\AopBundle\JMSAopBundle')) {
+        if ($config['use_tracer_interceptor'] === true && class_exists('JMS\AopBundle\JMSAopBundle')) {
             $this->loader->load('transport/amqp/tracer.xml');
         }
 
